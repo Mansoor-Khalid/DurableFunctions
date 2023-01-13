55f60185c382ea4b22b72246cf8db557cd4b9bfe
Mansoor-Khalid <mansoor_khalid@live.com>
2023-01-13 08:40:36 +0500

    fix

--- DurableFunctions/Program.cs
+++ DurableFunctions/Program.cs
@@ -3,20 +3,29 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureAppConfiguration(config =>
-    {
-        config
-            .AddJsonFile("appsettings.json", true, true)
-            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .Build();
-    })
-    .ConfigureServices(services =>
-    {
-        services.AddSingleton(sp => sp.GetRequiredService<ILoggerFactory>().CreateLogger("DefaultLogger"));
-    })
-    .Build();
 
-host.Run();
+namespace DurableFunctions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = new HostBuilder()
+        .ConfigureFunctionsWorkerDefaults()
+        .ConfigureAppConfiguration(config =>
+        {
+            config
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        })
+        .ConfigureServices(services =>
+        {
+            services.AddSingleton(sp => sp.GetRequiredService<ILoggerFactory>().CreateLogger("DefaultLogger"));
+        })
+        .Build();
+
+        host.Run();
+    }
+}
