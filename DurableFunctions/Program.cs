39704b1554a3982ef5634ff2fbb4368397b1b7d9
Mansoor-Khalid <mansoor_khalid@live.com>
2023-01-13 16:34:40 +0500

    fixes

--- DurableFunctions/Program.cs
+++ DurableFunctions/Program.cs
@@ -6,20 +6,20 @@ using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
 .ConfigureFunctionsWorkerDefaults()
-//#if DEBUG
-//.ConfigureAppConfiguration(config =>
-//{
-//    config
-//        .AddJsonFile("appsettings.json", true, true)
-//        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-//        .AddEnvironmentVariables()
-//        .Build();
-//})
-//#endif
-//.ConfigureServices(services =>
-//{
-//    services.AddSingleton(sp => sp.GetRequiredService<ILoggerFactory>().CreateLogger("DefaultLogger"));
-//})
+////#if DEBUG
+.ConfigureAppConfiguration(config =>
+{
+    config
+        .AddJsonFile("appsettings.json", true, true)
+        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+})
+////#endif
+.ConfigureServices(services =>
+{
+    services.AddSingleton(sp => sp.GetRequiredService<ILoggerFactory>().CreateLogger("DefaultLogger"));
+})
 .Build();
 
 host.Run();
